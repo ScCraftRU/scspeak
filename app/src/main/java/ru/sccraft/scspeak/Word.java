29349935417963b6df45e453a25a4fd82de72730
alexandr <admin@sccraft.ru>
2017-02-19 20:36:24 +0300

    ScSpeak

--- app/src/main/java/ru/sccraft/scspeak/Word.java
+++ app/src/main/java/ru/sccraft/scspeak/Word.java
@@ -1,5 +1,7 @@
 package ru.sccraft.scspeak;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -9,7 +11,7 @@ import com.google.gson.GsonBuilder;
  * Created by alexandr on 18.02.17.
  */
 
-public class Word {
+public class Word implements Parcelable {
     static final String LOG_TAG = "Word";
     public String en;
     public String mk;
@@ -24,6 +26,34 @@ public class Word {
     public String ruTranscriptionToEN;
     public String ruTranscriptionToMK;
 
+    public Word() {
+
+    }
+
+    protected Word(Parcel in) {
+        en = in.readString();
+        mk = in.readString();
+        ru = in.readString();
+        enTranscriptionToMK = in.readString();
+        enTranscriptionToRU = in.readString();
+        mkTranscriptionToEN = in.readString();
+        mkTranscriptionToRU = in.readString();
+        ruTranscriptionToEN = in.readString();
+        ruTranscriptionToMK = in.readString();
+    }
+
+    public static final Creator<Word> CREATOR = new Creator<Word>() {
+        @Override
+        public Word createFromParcel(Parcel in) {
+            return new Word(in);
+        }
+
+        @Override
+        public Word[] newArray(int size) {
+            return new Word[size];
+        }
+    };
+
     public String toJSON() {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
@@ -37,4 +67,34 @@ public class Word {
         Word w = gson.fromJson(JSON, Word.class);
         return w;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(en);
+        dest.writeString(mk);
+        dest.writeString(ru);
+        dest.writeString(enTranscriptionToMK);
+        dest.writeString(enTranscriptionToRU);
+        dest.writeString(mkTranscriptionToEN);
+        dest.writeString(mkTranscriptionToRU);
+        dest.writeString(ruTranscriptionToEN);
+        dest.writeString(ruTranscriptionToMK);
+    }
+
+    public static final Parcelable.Creator<Word> CREATOR = new Creator<Word>() {
+        @Override
+        public Word createFromParcel(Parcel source) {
+            return new Word(source);
+        }
+
+        @Override
+        public Word[] newArray(int size) {
+            return new Word[size];
+        }
+    };
 }
