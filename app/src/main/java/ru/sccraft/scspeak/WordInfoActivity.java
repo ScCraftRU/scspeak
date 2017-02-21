833c62243fd7400c6321fb8f475faab7e41e975b
alexandr <admin@sccraft.ru>
2017-02-21 21:06:17 +0300

    ScSpeak

--- app/src/main/java/ru/sccraft/scspeak/WordInfoActivity.java
+++ app/src/main/java/ru/sccraft/scspeak/WordInfoActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -11,17 +14,31 @@ import com.google.android.gms.ads.AdView;
 public class WordInfoActivity extends AppCompatActivity {
 
     Word w;
+    TextView thisLanguage, tvEN, tvMK, tvRU;
+    LinearLayout llEN, llMK, llRU;
+    Button bEN, bMK, bRU;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_info);
         w = getIntent().getParcelableExtra("word");
+        thisLanguage = (TextView) findViewById(R.id.wordInfo_thisLanguage);
 
         // Load an ad into the AdMob banner view.
         AdView adView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder()
                 .setRequestAgent("android_studio:ad_template").build();
         adView.loadAd(adRequest);
+        if (w == null) return;
+        switch (getString(R.string.getSystemLanguage)) {
+            case "en":
+                break;
+            case "mk":
+                break;
+            case "ru":
+                break;
+        }
     }
 
 
