c7e432a942450a108f5cd9d756a6668c0df808ae
alexandr <admin@sccraft.ru>
2017-02-22 19:38:10 +0300

    ScSpeak

--- app/src/main/java/ru/sccraft/scspeak/WordInfoActivity.java
+++ app/src/main/java/ru/sccraft/scspeak/WordInfoActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -24,21 +25,32 @@ public class WordInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_word_info);
         w = getIntent().getParcelableExtra("word");
         thisLanguage = (TextView) findViewById(R.id.wordInfo_thisLanguage);
+        llEN = (LinearLayout) findViewById(R.id.wordInfo_layoutEN);
+        llMK = (LinearLayout) findViewById(R.id.wordInfo_layoutMK);
+        llRU = (LinearLayout) findViewById(R.id.wordInfo_layoutRU);
+        bEN = (Button)findViewById(R.id.wordInfo_en);
+        bMK = (Button)findViewById(R.id.wordInfo_mk);
+        bRU = (Button)findViewById(R.id.wordInfo_ru);
 
         // Load an ad into the AdMob banner view.
         AdView adView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                .setRequestAgent("android_studio:ad_template").build();
+        AdRequest adRequest = new AdRequest.Builder().setRequestAgent("android_studio:ad_template").build();
         adView.loadAd(adRequest);
         if (w == null) return;
         switch (getString(R.string.getSystemLanguage)) {
             case "en":
+                llEN.setVisibility(View.GONE);
                 break;
             case "mk":
+                llMK.setVisibility(View.GONE);
                 break;
             case "ru":
+                llRU.setVisibility(View.GONE);
                 break;
         }
+        bEN.setText(w.en);
+        bMK.setText(w.mk);
+        bRU.setText(w.ru);
     }
 
 
