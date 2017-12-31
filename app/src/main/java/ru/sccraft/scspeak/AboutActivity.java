0f517936788307ae8de922277e96a9261f9e7202
alexandr <admin@sccraft.ru>
2017-12-31 16:05:24 +0300

    ScSpeak

--- app/src/main/java/ru/sccraft/scspeak/AboutActivity.java
+++ app/src/main/java/ru/sccraft/scspeak/AboutActivity.java
@@ -156,4 +156,16 @@ public class AboutActivity extends AppCompatActivity {
         Intent открыть_политику_конфиденциальности_в_браузере = new Intent(Intent.ACTION_VIEW, ссылка_на_политику_конфиденциальности);
         startActivity(открыть_политику_конфиденциальности_в_браузере);
     }
+
+    public void viewLicense(View view) {
+        Uri ссылка_на_лицензию = Uri.parse("https://github.com/sashaqwert/scspeak/blob/master/LICENSE");
+        Intent открыть_лицензию_в_браузере = new Intent(Intent.ACTION_VIEW, ссылка_на_лицензию);
+        startActivity(открыть_лицензию_в_браузере);
+    }
+
+    public void viewSourceCode(View view) {
+        Uri ссылка_на_исходный_код = Uri.parse("https://github.com/sashaqwert/scspeak");
+        Intent открыть_исходный_код_в_браузере = new Intent(Intent.ACTION_VIEW, ссылка_на_исходный_код);
+        startActivity(открыть_исходный_код_в_браузере);
+    }
 }
